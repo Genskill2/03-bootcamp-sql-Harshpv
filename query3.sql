11ca9f47a9f781b0a409412961577cae88aab2d1
Harshpv <84956241+Harshpv@users.noreply.github.com>
2021-06-20 10:43:33 +0530

    Create query3.sql

---
+++ query3.sql
@@ -0,0 +1,4 @@
+select t1.title,t3.name from books as t1
+inner join books_subjects as t2 on t1.id=t2.book
+inner join subjects as t3 on t2.subject =t3.id
+where t3.name in ("Politics","Technology");
